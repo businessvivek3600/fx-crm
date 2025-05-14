a9a253137ea03edfd94be1b5960fcd3b248d15a6
Rajput diksha singh <dikshatouchwood@gmail.com>
2025-05-14 18:00:07 +0530

    working on forget password

--- lib/controller/auth_controller.dart
+++ lib/controller/auth_controller.dart
@@ -457,7 +457,7 @@ class AuthController extends GetxController {
         "confirm_password": confPass,
       });
       final response = await dioClient.post(
-        ApiConst.verify_code,
+        ApiConst.change_password,
         data: formData,
         token: false,
       );
