13ec0ee1d519db0a9d48c56d38362f3e858d9c54
businessvivek3600 <businessvivek3600@gmail.com>
2025-06-17 17:24:42 +0530

    random

--- lib/main.dart
+++ lib/main.dart
@@ -57,6 +57,7 @@ class MyApp extends StatefulWidget {
 class _MyAppState extends State<MyApp> {
   @override
   Widget build(BuildContext context) {
+
     return MaterialApp.router(
       debugShowCheckedModeBanner: false,
       theme: ThemeUtils.lightTheme,
